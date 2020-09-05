6b9678315703a0c68be8936f4da51cb1d76fa0cf
SHIINASAMA <SHIINA_KAORU@OUTLOOK.COM>
2020-09-05 23:53:00 +0800

    lesson5

---
+++ lesson5/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void main(){
+    float PI = 3.14159;
+    int s, r = 5;
+    s = r * r * PI;
+    printf("s = %d\n",s);
+
+    double a = 2.2f;
+    int b = (int)a;
+    printf("%d\n",a);
+
+    printf("%d",3 / 4);
+}
