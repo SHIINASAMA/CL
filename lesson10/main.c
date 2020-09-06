10d9c1f32cbd2814866b97befed32ae90291a182
SHIINASAMA <SHIINA_KAORU@OUTLOOK.COM>
2020-09-06 15:44:34 +0800

    lesson10

---
+++ lesson10/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+void main(){
+    char c = 'k';
+    int i = 1,j = 2,k = 3;
+    float x = 3e+5,y = 0.85;
+    printf("%d,%d\n",'a'+5<c,-i-2*j>=k+1);
+    printf("%d,%d\n",1<j<5,x-5.25<=x+y);
+    printf("%d,%d\n",i+j+k==-2*j,k==j==i+5);
+}
